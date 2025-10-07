cea5407d86a3a45d1995843670d107c06d5cabce
Camilo <5060049+cemarta7@users.noreply.github.com>
2025-10-07 18:11:21 -0500

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,27 +18,14 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $environment = env('APP_ENV');
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
 
-        switch ($environment) {
-            case 'production':
-                $middleware->trustProxies(
-                    at: '*',
-                    headers: Request::HEADER_X_FORWARDED_FOR |
-                        Request::HEADER_X_FORWARDED_HOST |
-                        Request::HEADER_X_FORWARDED_PORT |
-                        Request::HEADER_X_FORWARDED_PROTO
-                );
-                break;
-
-            default:
-                // Local/Development Configuration
-                $middleware->trustProxies(
-                    at: ['127.0.0.1', '::1'],
-                    headers: Request::HEADER_X_FORWARDED_FOR |
-                        Request::HEADER_X_FORWARDED_PROTO
-                );
-        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
